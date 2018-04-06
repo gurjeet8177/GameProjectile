995386b6efae56bc7094df3e7076eb8d95cb67e4
gurjeet8177 <30376021+gurjeet8177@users.noreply.github.com>
2018-04-06 14:15:07 -0400

    initilized the projectile and hurdels

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody playerBox;
+
+    
+	void Start () {
+          
+        
+        playerBox.AddForce(0, 1000, 500);
+	}
+	
+	
+	void Update () {
+     
+    }
+}
---
+++ Assets/followCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour {
+
+
+    public Transform PlayerValues;
+    public Vector3 offset;
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = PlayerValues.position + offset;
+	}
+}
